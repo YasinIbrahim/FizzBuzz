ca2977b7569d0f9f1c26259f45254b7fc6369fa1
Yasin Ibrahim <yigames@googlemail.com>
2015-07-27 14:07:31 +0100

    1.0 - initial commit
    FizzBuzz application created by Test Driven Development using NUnit test
    framework. The Units Tests are included in separate TDD_Tests project,
    FizzBuzz project separate, both inlcuded in solution.

---
+++ FizzBuzz/FizzBuzz.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+public class FizzBuzz
+{
+    public FizzBuzz() { }
+
+    public string checkFizzBuzz(int a)
+    {
+        if ((a % 3 == 0) && (a % 5 == 0))
+        {
+            return "FizzBuzz";
+        }
+        else if (a % 3 == 0)
+        {
+            return "Fizz";
+        }
+        else if (a % 5 == 0)
+        {
+            return "Buzz";
+        }
+        else
+        {
+            return a.ToString();
+        }
+            
+    }
+
+}
---
+++ TDD_Tests/UnitTest1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+[TestFixture]
+public class NUnitTests
+{
+        
+    FizzBuzz f = new FizzBuzz();
+    
+    [Test]
+    public void NumberDivisibleBy3_ReturnFizz()
+    {
+        Assert.AreEqual("Fizz", f.checkFizzBuzz(3) );
+    }
+
+    [Test]
+    public void NumberDivisibleBy5_ReturnBuzz()
+    {
+        Assert.AreEqual("Buzz", f.checkFizzBuzz(5));
+    }
+
+    [Test]
+    public void NumberDivisibleBy3And5_ReturnFizzBuzz()
+    {
+        Assert.AreEqual("FizzBuzz", f.checkFizzBuzz(15));
+    }
+
+    [Test]
+    public void NumberNotDivisibleBy3Or5_ReturnNumber()
+    {
+        Assert.AreEqual("22", f.checkFizzBuzz(22));
+    }
+    
+}
